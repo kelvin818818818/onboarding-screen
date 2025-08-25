87938dcebe60c702f1c7382d6dcc9d1d09c6767c
kelvin818818818 <kelvin818818818@gmail.com>
2025-08-25 13:40:33 +0800

    Fix Checkmark Size/Position and Button States

--- src/components/OptionCard.tsx
+++ src/components/OptionCard.tsx
@@ -79,8 +79,8 @@ export const OptionCard: React.FC<OptionCardProps> = ({
             <motion.div
               className="bg-success-500 text-white rounded-full shadow-lg flex-shrink-0"
               style={{ 
-                width: '24px', 
-                height: '24px', 
+                width: '32px', 
+                height: '32px', 
                 display: 'flex', 
                 alignItems: 'center', 
                 justifyContent: 'center'
@@ -89,7 +89,7 @@ export const OptionCard: React.FC<OptionCardProps> = ({
               animate={{ scale: 1 }}
               transition={{ type: "spring", stiffness: 500, damping: 30 }}
             >
-              <svg style={{ width: '14px', height: '14px' }} fill="currentColor" viewBox="0 0 20 20">
+              <svg style={{ width: '20px', height: '20px' }} fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
               </svg>
             </motion.div>
